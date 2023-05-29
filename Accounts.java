29faeb22cb4632db236f9d99dfb113af793b523a
muthuviswanath <muthuviswanath@outlook.com>
2023-05-29 17:18:39 +0530

    added getters

---
+++ Accounts.java
@@ -0,0 +1,15 @@
+package com.rakuten;
+
+public class Accounts {
+
+
+
+public int getAccountsId(){
+	return this.accountsId;
+}
+
+public String getAccountsHolderName(){
+	return this.accountsHolderName;
+}
+
+}
